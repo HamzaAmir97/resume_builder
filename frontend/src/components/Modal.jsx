bacc4ca8a7867cffcca9d8ccc3cb81020c153696
Hmazah <alhamza2012@gmail.com>
2025-07-02 10:00:00 +0300

    fix  theme selectore layout

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -15,7 +15,7 @@ const Modal = ({
     if (!isOpen) return null;
 
     return (
-        <div className="fixed inset-0 z-50 flex justify-center items-center w-full h-full bg-black-400/80 backdrop-blur-3xl backdrop-opacity-90">
+        <div className="fixed inset-0 z-50 flex justify-center items-center w-full h-full bg-black-400/80 backdrop-blur-3xl backdrop-opacity-90 md:p-8 p-4">
             {/* Modal Content */}
             <div className={'relative flex flex-col bg-white shadow-lg rounded-lg overflow-x-hidden'}>
                 {/* Modal Header */}
