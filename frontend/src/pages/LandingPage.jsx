3ab9e637b88224c8db1a0598b9760dc7cdcf0f81
Hmazah <alhamza2012@gmail.com>
2025-07-13 14:24:58 +0300

    start building landing page

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,8 +1,60 @@
+import React, { useState } from 'react'
+import HERO_IMG from '../assets/hero-img.png'
+import { useNavigate } from 'react-router-dom';
 
 const LandingPage = () => {
-  return (
-    <div>LandingPage</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default LandingPage
+  const [openAuthModal, setOpenAuthModal] = useState(false);
+  const [currentPage, setCurrentPage] = useState("login");
+
+  const handleCTA = () => {};
+  return (
+    <div className='w-full min-h-full bg-white'>
+      <div className="container mx-auto px-4 py-6">
+       
+        {/* Header */}
+        <header className="flex justify-between items-center mb-16">
+          <div className="text-xl font-bold">Resume Builder</div>
+          <button
+            className="bg-purple-100 text-sm font-semibold text-black px-7 py-2.5"
+            onClick={() => setOpenAuthModal(true)}
+          >
+            Login / Sign Up
+          </button>
+        </header>
+
+        {/* Hero Content */}
+        <div className="flex flex-col md:flex-row items-center">
+          <div className="w-full md:w-1/2 pr-4 mb-8 md:mb-0">
+            <h1 className="text-5xl font-bold mb-6 leading-tight">
+              Build Your{" "}
+              <span className="text-transparent bg-clip-text bg-[radial-gradient(circle,#7182ff_0%,#3cff52_100%)] bg-[length:200%_200%] animate-text-shine">
+                Resume Effortlessly
+              </span>
+            </h1>
+            <p className="text-lg text-gray-700 mb-8 ">
+              Craft a standout resume in minutes with our smart and intuitive
+              resume builder.
+            </p>
+            <button
+              className="bg-black text-sm font-semibold text-white px-8 py-3 rounded-lg hover:bg-gray-800 transition-colors cursor-pointer"
+              onClick={handleCTA}
+            >
+              Get Started
+            </button>
+          </div>
+          <div className="w-full md:w-1/2">
+            <img
+              src={HERO_IMG}
+              alt="Hero Image"
+              className="w-full rounded-lg"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
