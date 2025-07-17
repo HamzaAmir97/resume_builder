aff6ed29d5592e43af16b62daac1902e6c793c05
Hmazah <alhamza2012@gmail.com>
2025-07-18 02:41:14 +0300

    Fix: renamed path param in express.static to avoid conflict with 'path' module

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,23 +1,28 @@
-export const BASE_URL = "http://localhost:8000";
-
 // utils/apiPaths.js
+
+// يتغير تلقائياً حسب البيئة
+export const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000" // أو 8000 إذا كنت تستخدمه محليًا
+    : import.meta.env.VITE_API_URL;
+
 export const API_PATHS = {
-    AUTH: {
-        REGISTER: "/api/auth/register", // Signup
-        LOGIN: "/api/auth/login", // Authenticate user & return JWT token
-        GET_PROFILE: "/api/auth/profile", // Get logged-in user details
-    },
+  AUTH: {
+    REGISTER: "/api/auth/register",
+    LOGIN: "/api/auth/login",
+    GET_PROFILE: "/api/auth/profile",
+  },
 
-    RESUME: {
-        CREATE: "/api/resume",             // POST - Create a new resume
-        GET_ALL: "/api/resume",             // GET - Get all resumes of logged-in user
-        GET_BY_ID: (id) => `/api/resume/${id}`, // GET - Get a specific resume
-        UPDATE: (id) => `/api/resume/${id}`,    // PUT - Update a resume
-        DELETE: (id) => `/api/resume/${id}`,    // DELETE - Delete a resume
-        UPLOAD_IMAGES: (id) => `/api/resume/${id}/upload-images`, // PUT - Upload Thumbnail and Resume profile im
-    },
+  RESUME: {
+    CREATE: "/api/resume",
+    GET_ALL: "/api/resume",
+    GET_BY_ID: (id) => `/api/resume/${id}`,
+    UPDATE: (id) => `/api/resume/${id}`,
+    DELETE: (id) => `/api/resume/${id}`,
+    UPLOAD_IMAGES: (id) => `/api/resume/${id}/upload-images`,
+  },
 
-    IMAGE: {
-        UPLOAD_IMAGE: "/api/auth/upload-image",
-    },
+  IMAGE: {
+    UPLOAD_IMAGE: "/api/auth/upload-image",
+  },
 };
--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { BASE_URL } from "./apiPaths";
+// apiPaths.js
+
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
